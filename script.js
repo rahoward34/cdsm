de7dcc56a819b986a52e39bd106bd1752686e43c
rahoward34 <116023080+rahoward34@users.noreply.github.com>
2022-12-14 14:06:48 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function turnOff(element) {
+    element.innerText = "I WAS THERE";
+}
+function changeImage(image) {
+    image.src="scaryjoe.webp" 
+}
+function over(element) {
+    element.src="666.mov"
+}
+function out(element) {
+    element.src="cdsm.MOV"
+}
